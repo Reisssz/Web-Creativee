1a5983c330cbf71b0f9590459bf589f3284073ea
Reisssz <pedrinzx7896@gmail.com>
2024-10-17 15:44:21 -0300

    att-project-web

--- src/App.js
+++ src/App.js
@@ -1,51 +1,35 @@
-import './App.css';
-import React from 'react';
-import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// Importando estilos e bibliotecas necessárias
+import './App.css'; // Importa o CSS para estilização
+import React from 'react'; // Importa o React
+import Slider from 'react-slick'; // Importa o componente Slider da biblioteca react-slick
+import "slick-carousel/slick/slick.css"; // Estilos para o carrossel
+import "slick-carousel/slick/slick-theme.css"; // Temas do carrossel
 
 // Componente do carrossel
 const Carousel = () => {
+  // Configurações do carrossel
   const settings = {
-    dots: true,
-    infinite: true,
-    speed: 1500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,          // Habilita a reprodução automática
-    autoplaySpeed: 2800,     // Tempo de transição entre slides (em milissegundos)
-    customPaging: (i) => (
-      <div style={{
-        width: '5px',
-        height: '5px',
-        borderRadius: '50%',
-        backgroundColor: '#f1f1f1', // Cor da bolinha
-        border: '1px solid #fff', // Borda da bolinha
-        margin: '0 5px',
-        cursor: 'pointer',
-      }} />
-    ),
-    appendDots: (dots) => (
-      <div style={{
-        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fundo dos dots
-        borderRadius: '5px',
-      }}>
-        <ul style={{ margin: '0' }}> {dots} </ul>
-      </div>
-    )
+    dots: true, // Habilita os pontos de navegação
+    infinite: true, // Permite rolagem infinita
+    speed: 1500, // Velocidade de transição
+    slidesToShow: 1, // Número de slides a serem mostrados
+    slidesToScroll: 1, // Número de slides a serem rolados
+    autoplay: true, // Habilita a reprodução automática
+    autoplaySpeed: 2800, // Tempo de transição entre slides (em milissegundos)
   };
 
   return (
+    // Contêiner do carrossel
     <div style={{ width: '90%', margin: 'auto' }}>
-      <Slider {...settings}>
+      <Slider {...settings}> {/* Aplica as configurações ao Slider */}
         <div>
-          <img src="bg.png" alt="Imagem 1" style={{ width: '100%' }} />
+          <img src="bg.png" alt="Imagem 1" style={{ width: '100%' }} /> {/* Slide 1 */}
         </div>
         <div>
-          <img src="bg2.png" alt="Imagem 2" style={{ width: '100%' }} />
+          <img src="bg2.png" alt="Imagem 2" style={{ width: '100%' }} /> {/* Slide 2 */}
         </div>
         <div>
-          <img src="bg3.png" alt="Imagem 3" style={{ width: '100%' }} />
+          <img src="bg3.png" alt="Imagem 3" style={{ width: '100%' }} /> {/* Slide 3 */}
         </div>
       </Slider>
     </div>
@@ -55,16 +39,30 @@ const Carousel = () => {
 // Componente principal App
 function App() {
   return (
-    <div className="App">
-      <header className="header">
-        <img className="logo" src="logo.png" width="118px" alt="Logo" />
-        <div className='neon-text'>SERVIR POR ALGO MAIOR</div>
-        <img className="logo2" src="lagoinha.png" width="130px" alt="Logo2" />
+    <div className="App"> {/* Contêiner principal */}
+      <header className="header"> {/* Cabeçalho da aplicação */}
+        <img className="logo" src="logo.png" width="118px" alt="Logo" /> {/* Logo principal */}
+        <div className='neon-text'>SERVIR POR ALGO MAIOR</div> {/* Texto em neon */}
+        <img className="logo2" src="lagoinha.png" width="120px" alt="Logo2" /> {/* Segundo logo */}
       </header>
+
       <Carousel /> {/* Incluindo o carrossel no App */}
-      <h1>ESCALAS</h1>
+
+      <h1>CULTOS</h1> {/* Título da seção */}
+
+      <div className="content"> {/* Contêiner para os boxes de culto */}
+        <div className='box1'>
+          <div className='text-box'>WORSHIP NIGHT</div> {/* Box 1 com texto */}
+        </div>
+        <div className='box2'>
+          <div className='text-box'>CULTO FÉ</div> {/* Box 2 com texto */}
+        </div>
+        <div className='box3'>
+          <div className='text-box'>CULTO LEGACY</div> {/* Box 3 com texto */}
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App; // Exporta o componente App como padrão
